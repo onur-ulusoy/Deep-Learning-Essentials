Fix weight initialization range and update batch size calculation for NN_torch class

basics/03_overfitting_regularization/nn.py
@@ -70,7 +70,7 @@ class NN:
         # Apply dropout mask to dz2
         dz2 *= self.dropout_mask2
         
-        self.dw2 = np.matmul(self.a1.T, dz2) / m + + self.l2_lambda/m * self.W2  # Shape: (hidden1_size, hidden2_size)
+        self.dw2 = np.matmul(self.a1.T, dz2) / m + self.l2_lambda/m * self.W2  # Shape: (hidden1_size, hidden2_size)
         self.db2 = np.sum(dz2, axis=0, keepdims=True) / m  # Shape: (1, hidden2_size)
 
         # Backpropagate to first hidden layer (apply derivative on z1)

basics/03_overfitting_regularization/nn_torch.py
@@ -27,9 +27,9 @@ class NN_torch(nn.Module):
         self.criterion = nn.MSELoss()
         
         # Initialize weights
-        nn.init.uniform_(self.fc1.weight, 0, 0.01)
-        nn.init.uniform_(self.fc2.weight, 0, 0.01)
-        nn.init.uniform_(self.fc3.weight, 0, 0.01)
+        nn.init.uniform_(self.fc1.weight, -0.01, 0.01)
+        nn.init.uniform_(self.fc2.weight, -0.01, 0.01)
+        nn.init.uniform_(self.fc3.weight, -0.01, 0.01)
         
         # Initialize biases
         nn.init.uniform_(self.fc1.bias, 0, 1)
@@ -62,7 +62,7 @@ class NN_torch(nn.Module):
         y_train = torch.tensor(y_train, dtype=torch.float32)
         print("y_train:",y_train[0])
 
-        m = X_train.shape[0]
+        m = y_train.shape[0]
 
         for epoch in range(epochs):
             # Zero the gradients

basics/03_overfitting_regularization/train.py
@@ -33,7 +33,7 @@ np.random.seed(41)
 network = NN(hp.input_size, hp.hidden1_size, hp.hidden2_size, hp.output_size, learning_rate=0.3, l2_lambda=0.01, dropout_p1=0.05, dropout_p2=0.05)
 
 # Initialize the PyTorch neural network
-network = NN_torch(hp.input_size, hp.hidden1_size, hp.hidden2_size, hp.output_size, learning_rate=0.3, l2_lambda=0.01, dropout_p1=0.05, dropout_p2=0.05)
+#network = NN_torch(hp.input_size, hp.hidden1_size, hp.hidden2_size, hp.output_size, learning_rate=0.3, l2_lambda=0.01, dropout_p1=0.05, dropout_p2=0.05)
 
 # Train the network
 network.train_model(X, y_train_scaled, epochs=50000)